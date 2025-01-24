7b5d064fecf2cc29bc8abcea14b3278b4d9c0bd3
Jared Cannon <jaredcannon9@gmail.com>
2025-01-24 13:28:27 -0600

    Remove unnecessary comment

--- src/Listeners/CreateRewindVersion.php
+++ src/Listeners/CreateRewindVersion.php
@@ -80,7 +80,6 @@ class CreateRewindVersion
                 $newValues = Arr::only($allAttributes, $attributesToTrack);
             }
 
-            /** @var Model $model */
             // Create the RewindVersion record
             $rewindVersion = RewindVersion::create([
                 'model_type' => $model->getMorphClass(),
